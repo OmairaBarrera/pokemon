b848c96758ab46da840a7442a14053d221ca8e4a
OmairaBarrera <lomaira.barrera@gmail.com>
2023-04-20 08:51:52 -0500

    feat: :sparkles: Add everyone of the pokemons with functions.
    functions that add pokemons with paramether of end.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-function getPockemon(done){
-    const resuls = fetch("https://pokeapi.co/api/v2/pokemon/1/");
+function getPockemon(id, done){
+    const resuls = fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
     resuls
         .then(response => response.json())
         .then(data => done(data))
 }
 
-getPockemon(data =>{
-    console.log(data.name)
-    document.querySelector('main').insertAdjacentHTML("beforeend",`
-        <div class="pockemon1">
-            <img src="${data.sprites.other.home.front_default}" alt="">
-            <h2> ${data.name} </h2>
-            <p> Habilidad </p>
-        </div>
-    `)
-});
+function pokemons(num){
+    for(let i=1; i<=num; i++){
+        getPockemon(i, data =>{
+            document.querySelector('main').insertAdjacentHTML("beforeend",`
+                <div class="pockemon1">
+                    <img src="${data.sprites.other.home.front_default}" alt="">
+                    <h2>${data.name}</h2>
+                </div>
+            `)
+        });
+    }
+}
+
+pokemons(500);
